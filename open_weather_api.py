94fcfb29082a7939450ac27b361a839ee6d6cefc
Long Lam <long.lam.1254@gmail.com>
2025-04-08 21:41:19 -0700

    Program will now parse API results to determine whether it will or is raining in Portland within the next 3 days

--- open_weather_api.py
+++ open_weather_api.py
@@ -9,12 +9,10 @@ base_url = "http://api.openweathermap.org/data/2.5/weather?"
 forecast_url = "http://api.openweathermap.org/data/2.5/forecast?"
 
 # grab city name from user
-city_name = input("Enter city name : ")
+city_name = "Portland"
 
-# complete_url variable to store
-# complete url address
 complete_url = base_url + "appid=" + api_key + "&q=" + city_name
-forecast_complete_url = forecast_url + "appid=" + api_key + "&q=" + city_name
+forecast_complete_url = forecast_url + "appid=" + api_key + "&q=" + city_name + ",OR,US"
 
 # get method of requests module
 # return response object
@@ -25,49 +23,42 @@ forecast_response = requests.get(forecast_complete_url)
 # convert json format data into
 # python format data
 weather_json = response.json()
-forecast_json = forecast_response()
+forecast_json = forecast_response.json()
 
 # Now x contains list of nested dictionaries
 # Check the value of "cod" key is equal to
 # "404", means city is found otherwise,
 # city is not found
-if x["cod"] != "404":
+if weather_json["cod"] != "404":
 
     # store the value of "main"
     # key in variable y
-    y = x["main"]
-
-    # store the value corresponding
-    # to the "temp" key of y
-    current_temperature = y["temp"]
-
-    # store the value corresponding
-    # to the "pressure" key of y
-    current_pressure = y["pressure"]
-
-    # store the value corresponding
-    # to the "humidity" key of y
-    current_humidity = y["humidity"]
+    y = weather_json["main"]
 
     # store the value of "weather"
     # key in variable z
-    z = x["weather"]
-
-    # store the value corresponding 
-    # to the "description" key at 
-    # the 0th index of z
-    weather_description = z[0]["description"]
+    z = weather_json["weather"]
 
     # store the value corresponding 
     # to the "main" key at 
     # the 0th index of z 
     weather = z[0]["main"]
 
+    # Determine if it is currently raining in Portland, OR
     print("A. Is it raining in Portland, OR right now?")
-    if(weather == "Clouds"):
+    if(weather == "Rain"):
         print("Yes.")
     else:
         print("No.")
     print("Here is the current weather: ", weather)
 
-    print("\n",x)
+    willRain = False
+    print("Is it forecasted to be raining in Portland within the next three days?")
+    for date in forecast_json["list"]:
+        if date["weather"][0]["main"] == "Rain":
+            willRain = True
+            print("It will rain on this day: ", date["dt_txt"])
+
+    if willRain == False:
+        print("It will not rain in Portland, OR within the next three days.")
+        
