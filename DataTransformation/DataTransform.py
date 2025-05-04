3be1d53dbd498bc4cb3aae61ef711017afbfd8bd
Long Lam <long.lam.1254@gmail.com>
2025-05-03 22:10:37 -0700

    Cleanup

--- DataTransformation/DataTransform.py
+++ DataTransformation/DataTransform.py
@@ -15,10 +15,7 @@ df = df.drop(columns=['EVENT_NO_STOP', 'GPS_SATELLITES', 'GPS_HDOP'])
 print("drop() result: \n", df.head())
 
 # 2C. Use the usecols parameter
-# First, get all column names (without loading full data)
 all_columns = pd.read_csv('bc_trip259172515_230215.csv', nrows=0).columns.tolist()
-
-# Filter out unwanted columns
 columns_to_use = [col for col in all_columns if col not in ['EVENT_NO_STOP', 'GPS_SATELLITES', 'GPS_HDOP']]
 
 # Load the CSV file with selected columns
@@ -27,16 +24,15 @@ df_filtered = pd.read_csv('bc_trip259172515_230215.csv', usecols=columns_to_use)
 print("usecols result: \n", df_filtered.head())
 
 # 3. Decode
-# Decode timestamp
+# 3Ai., 3Aii., and 3Aiii.
 def decode_timestamp(row):
     base_date = datetime.strptime(row['OPD_DATE'], "%d%b%Y:%H:%M:%S")  # Convert to datetime
     time_offset = timedelta(seconds=row['ACT_TIME'])
     return base_date + time_offset
 
-# Apply the function row-wise
 df_filtered['TIMESTAMP'] = df_filtered.apply(decode_timestamp, axis=1)
 
-# Drop OPD_DATE and ACT_TIME
+# 3B. Drop OPD_DATE and ACT_TIME
 df_decoded = df_filtered.drop(columns=['OPD_DATE', 'ACT_TIME'])
 
 print("Remaining columns after decoding timestamp: \n", df_decoded.columns.tolist())
@@ -44,17 +40,17 @@ print("Resulting Table: \n", df_decoded.head())
 
 
 # 4. Enhance
-# 4i. Utilize the pandas.DataFrame.diff() method for this calculation. 
+# 4Ai. Utilize the pandas.DataFrame.diff() method for this calculation. 
 df_decoded['dMETERS'] = df_decoded['METERS'].diff()
 df_decoded['dTIMESTAMP'] = df_decoded['TIMESTAMP'].diff().dt.total_seconds()
 
-# 4ii. use apply() (with a lambda function) to calculate SPEED = dMETERS / dTIMESTAMP.
+# 4Aii. use apply() (with a lambda function) to calculate SPEED = dMETERS / dTIMESTAMP.
 df_decoded['SPEED'] = df_decoded.apply(
     lambda row: row['dMETERS'] / row['dTIMESTAMP'] if row['dTIMESTAMP'] and row['dTIMESTAMP'] > 0 else 0,
     axis=1
 )
 
-# 4iii. Filter/drop the unneeded dMETERS and dTIMESTAMP columns.
+# 4Aiii. Filter/drop the unneeded dMETERS and dTIMESTAMP columns.
 df_final = df_decoded.drop(columns=['dMETERS', 'dTIMESTAMP'])
 
 print(f"Minimum speed: {df_final['SPEED'].min():.2f}")
